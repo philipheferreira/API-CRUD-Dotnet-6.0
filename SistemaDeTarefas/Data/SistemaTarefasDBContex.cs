68887632bac66bf8a4c63785cef0e37a3eecabf3
philipheferreira <philipheferreira@gmail.com>
2024-06-19 17:22:56 -0300

    Upgrade: date classes

---
+++ SistemaDeTarefas/Data/SistemaTarefasDBContex.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaDeTarefas.Data{
+    public class SistemaTarefasDBContex : DbContext {
+
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> options) : base(options){ 
+        
+        }
+
+        public DbSet<Models.UsuarioModel> Usuarios { get; set; }
+    }
+}
--- SistemaDeTarefas/obj/Debug/net6.0/SistemaDeTarefas.AssemblyInfo.cs
+++ SistemaDeTarefas/obj/Debug/net6.0/SistemaDeTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eacc0d0f7ae31863eceddfbd172454fc7d59b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
